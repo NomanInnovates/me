97e9c827b8b7f254b82d5e36f3211c5b12bbccff
Nomi0125 <nomansandhu0125@gmail.com>
2022-07-17 00:46:07 +0500

    some change

--- js/textRotate.js
+++ js/textRotate.js
@@ -211,9 +211,9 @@ jQuery(document).ready(function($){
 		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
-		lettersDelay = 50,
+		lettersDelay = 550,
 		//type effect
-		typeLettersDelay = 150,
+		typeLettersDelay = 5150,
 		selectionDuration = 500,
 		typeAnimationDelay = selectionDuration + 800,
 		//clip effect
